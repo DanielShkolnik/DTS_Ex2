de867e7c64e71250bbc2405de921bb489bb92e65
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-12-30 21:08:47 +0200

    new

--- dataCenter.cpp
+++ dataCenter.cpp
@@ -5,3 +5,15 @@ int DataCenter::getID(){
     return this->DC_ID;
 }
 
+DataCenter* DataCenter::getNext(){
+    return this->next;
+}
+
+void DataCenter::setNext(DataCenter* next){
+    this->next = next;
+}
+
+std::shared_ptr<DataCenterGroup> DataCenter::getGroup(){
+    return this->group;
+}
+
