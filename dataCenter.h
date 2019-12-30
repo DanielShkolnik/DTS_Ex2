84808f3c7f7f0dd337a29d3c09772c6b744f2f7e
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-12-30 18:37:04 +0200

    new

--- dataCenter.h
+++ dataCenter.h
@@ -7,38 +7,14 @@ class DataCenter{
 private:
     int DC_ID;
     DataCenter* next;
-    DataCenterGroup
+    std::shared_ptr<DataCenterGroup>* group;
 
-
-
-
-
-    std::shared_ptr<Node<int,Server>>* servers_array;
-    std::shared_ptr<Node<int,Server>> linux_free_head;
-    std::shared_ptr<Node<int,Server>> linux_free_tail;
-    std::shared_ptr<Node<int,Server>> windows_free_head;
-    std::shared_ptr<Node<int,Server>> windows_free_tail;
-    int num_of_linux;
-    int num_of_windows;
-    int num_of_servers;
-    void remove_from_list(int OS, int ID);
-    int getAndRemoveFreeServer(int OS);
-    void appendToList(int OS, std::shared_ptr<Node<int,Server>>& server_ptr);
-    void deleteList();
-    void copyArrayAndList(const DataCenter& dc);
 public:
     int getID();
-    DataCenter(int ID, int num_of_servers);
+    DataCenter(int ID);
     ~DataCenter();
     DataCenter& operator=(const DataCenter& dc) = delete;
     DataCenter(const DataCenter& dc) = delete;
-    int getNumOfLinux();
-    int getNumOfWindows();
-    int assignServer(int server_ID, int OS);
-    void freeServer(int server_ID);
-    class InvalidServerID{};
-    class AllServersAreTaken{};
-    class ServerIsAlreadyFree{};
 };
 
 
