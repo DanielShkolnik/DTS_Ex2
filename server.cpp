5fdf042b7183f0620dc4928fc84c26a3e8f1a7fc
OmerHemo <OmerHemo@users.noreply.github.com>
2019-12-30 19:09:20 +0200

    add server

---
+++ server.cpp
@@ -0,0 +1,12 @@
+//
+// Created by omer on 30/12/2019.
+//
+
+#include "server.h"
+
+void Server::setTraffic(int traffic){
+    this->traffic = traffic;
+}
+ int Server::getTraffic() {
+     return this->traffic;
+}
---
+++ server.h
@@ -0,0 +1,17 @@
+//
+// Created by omer on 30/12/2019.
+//
+
+#ifndef DTS_EX2_SERVER_H
+#define DTS_EX2_SERVER_H
+class Server{
+private:
+    int server_id;
+    int traffic;
+    int DC_id;
+public:
+    Server(int server_id, int DC_id):server_id(server_id), traffic(0), DC_id(DC_id){};
+    void setTraffic(int traffic);
+    int getTraffic();
+};
+#endif //DTS_EX2_SERVER_H
