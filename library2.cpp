17f61c2648c76462a68bfa6728535a34a9b5cf5e
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-01-14 19:13:22 +0200

    new

--- library2.cpp
+++ library2.cpp
@@ -4,7 +4,7 @@
 
 void* Init(int n){
     if(n>0){
-        DataCenterManager* DS = new DataCenterManager(n);
+        DataCenterManager* DS = new DataCenterManager(n+1);
         return (void*)DS;
     }
     else return nullptr;
